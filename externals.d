0915794276eef42655169fd0a300ea7b89f4582f
Zacharý <zacharyacutey@users.noreply.github.com>
2017-05-31 17:51:23 -0400

    Update externals.d

--- externals.d
+++ externals.d
@@ -1,3 +1,17 @@
 //The file with all the external C functions from ncurses.
 extern(C) {
+	struct WINDOW;
+	WINDOW* initscr();
+	int color_pair(int);
+	int attr_on(int);
+	int mvaddstr(int,int,const char*);
+	int attr_off(int);
+	int wresize(WINDOW*,int,int);
+	int getch();
+	void endwin();
+	int start_color();
+	int init_pair(short,short,short);
+	void clear();
+	int noecho();
+	int curs_set(int);
 }
